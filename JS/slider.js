d81e7a190ea6ab4059b4c2cad1b57786c2d62ae2
fredesse <eleegia@gmail.com>
2017-11-12 16:50:03 -0800

    Add prev and next buttons in md screen size & footer

--- JS/slider.js
+++ JS/slider.js
@@ -1,26 +1,26 @@
 const slides = document.getElementsByClassName("slides-batman");
 const dots = document.getElementsByClassName("dot");
-let slideIndex = 1;
+let imgIndex = 1;
 
-navigateSlider = (n) => {
-  showSlide(slideIndex += n);
+navigateImages = (n) => {
+  showImage(imgIndex += n);
 };
 
-currentSlide = (n) => {
-  showSlide(n);
+currentImage = (n) => {
+  showImage(imgIndex = n);
 };
 
-showSlide = (n) => {
-  if (n > slides.length) { slideIndex = 1; }
-  if (n < 1) { slideIndex = slides.length; }
+showImage = (n) => {
+  if (n > slides.length) { imgIndex = 1; }
+  if (n < 1) { imgIndex = slides.length; }
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (let i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  slides[imgIndex - 1].style.display = "block";
+  dots[imgIndex - 1].className += " active";
 };
 
-showSlide(slideIndex);
+showImage(imgIndex);
